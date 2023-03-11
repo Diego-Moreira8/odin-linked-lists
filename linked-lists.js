24fe34b3955f335f755144da8e24155698a5a6d6
Diego Moreira <diego.moreira529@gmail.com>
2023-03-10 21:02:41 -0300

    Removing tests

--- linked-lists.js
+++ linked-lists.js
@@ -193,23 +193,3 @@ class Node {
     this.next = _next;
   }
 }
-
-// Creating nodes
-let node1 = new Node("Diego");
-let node2 = new Node("Paloma");
-let node3 = new Node("Rafaello");
-let node4 = new Node("Lorena");
-
-// Adding the links
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-
-// Creating the Linked List with the first node as the head
-let list = new LinkedList(node1);
-
-// console.log(node1);
-// console.log(node2);
-// console.log(node3);
-
-console.log(list.toString());
